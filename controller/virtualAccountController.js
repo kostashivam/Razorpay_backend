6756efa4cca86979c21b5e882fc5fa301abb0001
Shivamkosta <shivamkosti570@gmail.com>
2022-05-01 10:52:41 +0530

    Initial commit

--- controller/virtualAccountController.js
+++ controller/virtualAccountController.js
@@ -6,30 +6,33 @@ const fetch = require("node-fetch");
 const virtual_accounts = async (req, res) => {
   try {
     const { customer_name, description } = req.body;
-    var instance = new Razorpay({
-      key_id: process.env.KEY_ID,
-      key_secret: process.env.SECRETE_KEY,
-    });
-    const virtualAccount = await instance.virtualAccounts.create({
-      receivers: {
-        types: ["bank_account"],
-      },
-      description: description,
-      customer_id: process.env.CUSTOMER_ID,
-      close_by: 1681615838,
-      notes: {
-        project_name: "Banking Software",
-      },
-    });
-    const createVirtualAccounts = new virtualAccountModel({
-      _id: virtualAccount.id,
-      customer_id: process.env.CUSTOMER_ID,
-      customer_name: customer_name,
-      description: description,
-    });
-    const vaccount = await createVirtualAccounts.save();
-    // console.log("vaccount", vaccount);
-    return res.status(201).json({ data: vaccount });
+    if (req.body != "") {
+      var instance = new Razorpay({
+        key_id: process.env.KEY_ID,
+        key_secret: process.env.SECRETE_KEY,
+      });
+      const virtualAccount = await instance.virtualAccounts.create({
+        receivers: {
+          types: ["bank_account"],
+        },
+        description: description,
+        customer_id: process.env.CUSTOMER_ID,
+        close_by: 1681615838,
+        notes: {
+          project_name: "Banking Software",
+        },
+      });
+      const createVirtualAccounts = new virtualAccountModel({
+        _id: virtualAccount.id,
+        customer_id: process.env.CUSTOMER_ID,
+        customer_name: customer_name,
+        description: description,
+      });
+      const vaccount = await createVirtualAccounts.save();
+      return res.status(201).json({ data: vaccount });
+    } else {
+      return res.status(404).json({ Invalid: "Invalid field" });
+    }
   } catch (error) {
     console.log("error: ", error);
     res.status(400).json({ message: "Bad Request Error" });
@@ -40,14 +43,18 @@ const virtual_accounts = async (req, res) => {
 const closeVirtualAccount = async (req, res) => {
   try {
     let account_id = req.params.account_id;
-    let url = `${process.env.RAZORPAY_URL}/virtual_accounts/${account_id}/close`;
-    let resp = await fetch(url, {
-      method: "POST",
-      headers: {
-        Authorization: `Basic ${process.env.TOKEN}`,
-      },
-    });
-    return res.status(200).json(resp);
+    if (account_id) {
+      let url = `${process.env.RAZORPAY_URL}/virtual_accounts/${account_id}/close`;
+      let resp = await fetch(url, {
+        method: "POST",
+        headers: {
+          Authorization: `Basic ${process.env.TOKEN}`,
+        },
+      });
+      return res.status(200).json(resp);
+    } else {
+      return res.status(404).json({ error: "Account Id is not found" });
+    }
   } catch (error) {
     console.log("err: ", error);
     return res.status(401).json({ error: "Bad Request" });
@@ -61,10 +68,14 @@ const fetchAllVirtualAccounts = async (req, res) => {
       key_id: process.env.KEY_ID,
       key_secret: process.env.SECRETE_KEY,
     });
-    const allVirtualAccounts = await instance.virtualAccounts.all();
-    return res.status(200).json({ virtualAccounts: allVirtualAccounts });
+    if (true) {
+      const allVirtualAccounts = await instance.virtualAccounts.all();
+      return res.status(200).json({ virtualAccounts: allVirtualAccounts });
+    } else {
+      return res.status(404).json({ error: "Not found data" });
+    }
   } catch (error) {
-    return res.status(404).json({ error: "Request failed" });
+    return res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -96,22 +107,26 @@ const webhook = async (req, res) => {
     let amount_paid = req.body.payload.virtual_account.entity.amount_paid;
     let method = req.body.payload.payment.entity.method;
 
-    const data = await virtualAccountModel.findOneAndUpdate(
-      { _id: virtual_id },
-      {
-        $set: {
-          amount: amount_paid / 100,
-          method: method,
+    if (virtual_id) {
+      const data = await virtualAccountModel.findOneAndUpdate(
+        { _id: virtual_id },
+        {
+          $set: {
+            amount: amount_paid / 100,
+            method: method,
+          },
+          $currentDate: { lastModified: true },
         },
-        $currentDate: { lastModified: true },
-      },
-      { new: true, useFindAndModify: false }
-    );
-    console.log("data: ", data);
-    return res.status(200).json({
-      message: "Updated amount",
-      data: data,
-    });
+        { new: true, useFindAndModify: false }
+      );
+      console.log(data);
+      return res.status(200).json({
+        message: "Updated amount",
+        data: data,
+      });
+    } else {
+      return res.status(404).json({ Invalid: "Invalid virutal id" });
+    }
   } catch (error) {
     console.log(error);
   }
